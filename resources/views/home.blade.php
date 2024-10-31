aa85b3c44fe974364a732e17bb31a4c77c17ab67
Sehan Diki Triansyah <sehandikitriansyah@MacBook-Pro-Yudi.local>
2024-10-31 22:57:15 +0700

    add new update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -615,8 +615,8 @@
     </section>
     <!-- /Testimonials Section -->
 
-    <!-- Layanan kami section -->
-      <div class="container section-title" data-aos="fade-up">
+<!-- Layanan kami section -->
+<div class="container section-title" data-aos="fade-up">
         <h2>Hubungi Kami</h2>
         <p>Kami siap membantu Anda dengan segala kebutuhan renovasi dan pembangunan rumah. Jangan ragu untuk menghubungi kami!</p>
       </div><!-- End Section Title -->
@@ -630,24 +630,24 @@
     <div class="col-lg-6">
       <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="200">
         <i class="bi bi-geo-alt"></i>
-        <h3>Address</h3>
-        <p>A108 Adam Street, New York, NY 535022</p>
+        <h3>Alamat</h3>
+        <p>Rangkasbitung Lebak Banten</p>
       </div>
     </div><!-- End Info Item -->
 
     <div class="col-lg-3 col-md-6">
       <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="300">
         <i class="bi bi-telephone"></i>
-        <h3>Call Us</h3>
-        <p>+1 5589 55488 55</p>
+        <h3>Hubungi Kami</h3>
+        <p>081318758831</p>
       </div>
     </div><!-- End Info Item -->
 
     <div class="col-lg-3 col-md-6">
       <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="400">
         <i class="bi bi-envelope"></i>
-        <h3>Email Us</h3>
-        <p>info@example.com</p>
+        <h3>Email Kami</h3>
+        <p>TukangBanten@gmail.com</p>
       </div>
     </div><!-- End Info Item -->
 
@@ -655,7 +655,7 @@
 
   <div class="row gy-4 mt-1">
     <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.354887429953!2d106.25091927593708!3d-6.348073193641771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e4210cd43fe4189%3A0x4aa6245cde7a84d4!2sJl.%20Kh%20Atim%2C%20Kec.%20Rangkasbitung%2C%20Kabupaten%20Lebak%2C%20Banten!5e0!3m2!1sid!2sid!4v1730389688031!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div><!-- End Google Maps -->
 
     <div class="col-lg-6">
