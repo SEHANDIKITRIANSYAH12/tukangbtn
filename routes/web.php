f0b95b4fd473328c983feae35324f1c3ccb8a5fd
Yudi Eka Permana <yudi.permana@bcadigital.co.id>
2025-01-26 14:54:57 +0700

    fiix

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Home routes
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/home', function () {
-    return redirect('/');
+Route::get('', function () {
+    return view('home'); // Ganti 'welcome' dengan view yang sesuai
 });
 
-// Service routes
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/jasa1', function () {
     return view('jasa1');
-})->name('jasa1');
+});
 
 Route::get('/jasa2', function () {
     return view('jasa2');
-})->name('jasa2');
+});
 
 Route::get('/jasa3', function () {
     return view('jasa3');
-})->name('jasa3');
+});
 
 Route::get('/jasa4', function () {
     return view('jasa4');
-})->name('jasa4');
+});
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact'); // Ganti dengan nama view Anda
 })->name('contact');
 
 Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
